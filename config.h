3d1021e9039bbb1c9d118ecaa3d87330ee31bc97
niraj <niraj.apr4@gmail.com>
2026-03-21 23:41:40 +0530

    updated font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Aporetic Sans Mono:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "AporeticSansMonoNerdFont:pixelsize=16:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
